0355e0425e188630cf9cb14a45b281c892feace1
D[oa]vid Weisz <dave@dweisz.com>
2026-04-14 19:52:57 +0000

    New integration tests - untested

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   testEnvironment: "node",
   roots: ["<rootDir>/src"],
   testMatch: ["**/*.test.ts"],
+  testPathIgnorePatterns: ["\\.integration\\.test\\.ts$"],
   transform: {
     "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.test.json" }],
   },
